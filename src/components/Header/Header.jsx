a4f876360a6af853b833ab85a69edc45d1026df2
LivioDR <liviodavidreinoso@yahoo.com.ar>
2024-06-07 16:31:02 -0400

    Metadata and Header components created

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './HeaderStyle.css'
+
+const Header = () => {
+    return(
+        <>
+        <div>
+            <h1>PokeBattle Simulator</h1>
+        </div>
+        </>
+    )
+}
+export default Header
---
+++ src/components/Metadata.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Metadata = () => {
+    return(
+        <>
+            <meta charSet="UTF-8"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+            <link rel="manifest" href="manifest.json" crossOrigin="use-credentials"/>
+            <link rel="icon" type="image/x-icon" href="/favicon.ico"/>
+            <meta name="theme-color" content="#3578D0"/>
+        </>
+    )
+}
+export default Metadata
