530f1b8957834fedc7fb0de1f9332c521229569c
LivioDR <liviodavidreinoso@yahoo.com.ar>
2024-06-08 19:13:55 -0400

    selection page added. Pokemon list event triggered

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,27 @@
+'use client'
+
 import Metadata from "@/components/Metadata";
 import Header from "@/components/Header/Header";
+import SelectionPage from "@/pages/SelectionPage";
+import { useState, useEffect } from "react";
+import getAllPokemon from "@/services/getAllPokemon";
 
 export default function Home() {
+
+  const [pokeList, setPokeList] = useState()
+
+
+  useEffect(()=>{
+    setPokeList(getAllPokemon())
+  },[])
+
+
+
   return (
     <>
       <Metadata/>
       <Header/>
+      <SelectionPage pokeList={pokeList}/>
     </>
   );
 }
