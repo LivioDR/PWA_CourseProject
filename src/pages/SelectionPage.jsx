d364516c26ec3c8166365133ae167641f2627932
LivioDR <liviodavidreinoso@yahoo.com.ar>
2024-06-08 19:13:22 -0400

    stats management added

--- src/pages/SelectionPage.jsx
+++ src/pages/SelectionPage.jsx
@@ -1,23 +1,25 @@
 import PokeStats from "@/components/PokeStats/PokeStats";
+import PokeImage from "@/components/PokeImage/PokeImage";
 import React, { useEffect, useState } from "react";
 
 const SelectionPage = ({pokeList}) => {
 
     const [poke, setPoke] = useState()
+    const [stats, setStats] = useState([])
 
     useEffect(()=>{
+        setStats(poke?.stats)
         
-
-
     },[poke])
 
+    console.log(pokeList)
 
     return(
         <>
-            <PokeImage/>
-            <PokeSelector pokeList={pokeList} setPoke={setPoke} />
+            <PokeImage img={'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/132.gif'}/>
+            {/* <PokeSelector pokeList={pokeList} setPoke={setPoke} /> */}
             <PokeStats/>
-            <PokeMoves/>
+            {/* <PokeMoves/> */}
         </>
     )
 }
