16e4a9ed0fc4b83e98a9eda3389c1719e5b03e59
LivioDR <liviodavidreinoso@yahoo.com.ar>
2024-06-08 15:42:47 -0400

    StatBar & Container done

---
+++ src/components/PokeStats/PokeStats.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import StatBar from "./StatBar/StatBar";
+
+const containerStyle = {
+    width: '400px',
+    marginTop: '20px',
+    height: '200px',
+}
+
+
+const PokeStats = ({stats}) => {
+
+    return(
+        <div style={containerStyle}>
+            {stats.map(stat => <StatBar name={stat.name} value={stat.value} key={stat.value + stat.name}/>)}
+        </div>
+    )
+}
+export default PokeStats
---
+++ src/components/PokeStats/StatBar/StatBar.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+const styles = {
+    container: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    statName: {
+        fontSize: '0.5em',
+        width: '10%',
+    },
+    barContainer: {
+        width: '80%',
+    },
+    statBar: {
+        height: '0.5em',
+    },
+    statNumber :{
+        width: '5%',
+        fontSize: '0.5em',
+    }
+}
+
+
+const StatBar = ({name, value, maxValue}) => {
+
+    const percentage = (value/maxValue)*100
+
+    return(
+        <div style={styles.container}>
+            <p style={styles.statName}>{name}</p>
+            <div style={styles.barContainer}>
+                <div style={{...styles.statBar, width:`${percentage}%`, backgroundColor:hsl(`${percentage}deg 60% 45%`)}}> 
+                </div>
+            </div>
+            <p style={styles.statNumber}>{value}</p>
+        </div>
+    )
+}
+export default StatBar
