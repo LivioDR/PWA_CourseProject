8be56eab068ed178faf160bfbe87f351f47c04d5
LivioDR <liviodavidreinoso@yahoo.com.ar>
2024-06-11 12:32:10 -0400

    completed component functionality. Added interactivity. Raised placeholder to parent component

--- src/components/PokeCardContainer/PokeCardContainer.jsx
+++ src/components/PokeCardContainer/PokeCardContainer.jsx
@@ -10,40 +10,12 @@ const containerStyle = {
 }
 
 
-const PokeCardContainer = ({pokeList}) => {
-
-    const placeholder = [
-        {
-            name: 'bulbasaur',
-            image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/1.gif',
-            level: 15,
-            exp: 2400,
-        },
-        {
-            name: 'charmander',
-            image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/4.gif',
-            level: 5,
-            exp: 150,
-        },
-        {
-            name: 'squirtle',
-            image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/7.gif',
-            level: 17,
-            exp: 3500,
-        },
-        {
-            name: 'pikachu',
-            image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/25.gif',
-            level: 25,
-            exp: 12000,
-        }
-    ]
-
+const PokeCardContainer = ({pokeList, selectedPokemon, setSelectedPokemon, setPokemonData}) => {
 
 
     return(
         <div style={containerStyle}>
-        {placeholder.map(poke =><PokeCard key={poke.exp} name={poke.name} lvl={poke.level} exp={poke.exp} image={poke.image}/>)}
+        {pokeList.map(poke =><PokeCard key={poke.id} id={poke.id} name={poke.name} lvl={poke.level} exp={poke.exp} image={poke.image} selected={selectedPokemon} setSelected={setSelectedPokemon} setPokemonData={setPokemonData}/>)}
         </div>
     )
 }
