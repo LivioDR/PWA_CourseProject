266abdb3877a07831e0834d6bf0f173b1e57e868
LivioDR <liviodavidreinoso@yahoo.com.ar>
2024-06-11 12:31:23 -0400

    added interactivity

--- src/components/PokeCard/PokeCard.jsx
+++ src/components/PokeCard/PokeCard.jsx
@@ -1,7 +1,9 @@
+'use client'
 import React from "react";
 import { getCurrentLevel } from "@/services/battleLogic";
 import NameLevelContainer from "../NameLevelContainer/NameLevelContainer";
 import PokeImage from "../PokeImage/PokeImage";
+import { getPokemonCry, getPokemonData } from "@/services/getPokemonData";
 
 const cardStyle = {
     container: {
@@ -10,9 +12,11 @@ const cardStyle = {
         flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',    
-        width: '40%',
-        maxHeight: '30%',
+        minWidth: '40%',
+        maxWidth: 350,
+        maxHeight: '40%',
         margin: '1%',
+        borderRadius: 20,
     },
     image: {
         width: '50%',
@@ -20,14 +24,26 @@ const cardStyle = {
     }
 }
 
-const PokeCard = ({name, lvl, exp, image}) => {
+const PokeCard = ({id, name, lvl, exp, image, selected, setSelected, setPokemonData}) => {
+
+    const toggleSelection = async() => {
+        setSelected(id)
+        const cry = await getPokemonCry(id)
+        const cryAudio = new Audio(cry)
+        cryAudio.volume = 0.5
+        cryAudio.play()
+
+        const allData = await getPokemonData(id)
+        console.warn(allData)
+        setPokemonData(allData)
+    }
 
     const currLevelExp = getCurrentLevel(lvl)
     const expNextLevel = getCurrentLevel(lvl+1)
     const percentageToNextLevel = ((exp - currLevelExp)/(expNextLevel - currLevelExp)*100)
 
     return(
-        <div style={cardStyle.container}>
+        <div style={{...cardStyle.container, backgroundColor: selected == id ? 'lightgrey' : ''}} onClick={()=>{toggleSelection()}}>
             <PokeImage img={image} style={cardStyle.image}/>
             <NameLevelContainer name={name} level={lvl} expPercentage={percentageToNextLevel}/>
         </div>
