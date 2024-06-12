045678a3860c828634082a9d1511d2778782d1cc
LivioDR <liviodavidreinoso@yahoo.com.ar>
2024-06-12 17:29:08 -0400

    header styling changed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import './HeaderStyle.css'
+import Image from "next/image";
 
 const Header = () => {
     return(
         <>
-        <div>
-            <h1>PokeBattle Simulator</h1>
+        <div className="headerContainer">
+            <Image
+                src={'/logo192.png'}
+                width={50}
+                height={50}
+                alt="App icon"
+                className="image"
+            />   
+            <h1 className="headerTitle">PokeBattle RPG</h1>
         </div>
         </>
     )
