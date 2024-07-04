a4a30a150322e85fe34bd97383c2468d313bb8d7
LivioDR <liviodavidreinoso@yahoo.com.ar>
2024-07-04 15:35:25 -0400

    refactored structure for faster indexing

--- src/utilities/StartingPokemonTeam.js
+++ src/utilities/StartingPokemonTeam.js
@@ -1,31 +1,27 @@
-const StartingPokemonTeam = [
-    {
-        id: 1,
+const StartingPokemonTeam = {
+    1: {
         name: 'bulbasaur',
         image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/1.gif',
         level: 5,
         exp: 135,
     },
-    {
-        id: 4,
+    4: {
         name: 'charmander',
         image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/4.gif',
         level: 5,
         exp: 135,
     },
-    {
-        id: 7,
+    7: {
         name: 'squirtle',
         image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/7.gif',
         level: 5,
         exp: 135,
     },
-    {
-        id: 25,
+    25: {
         name: 'pikachu',
         image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/25.gif',
         level: 5,
         exp: 135,
     }
-]
+}
 export default StartingPokemonTeam
