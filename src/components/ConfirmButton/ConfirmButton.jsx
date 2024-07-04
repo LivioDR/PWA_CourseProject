a49c44b03a4a54802ed57f9891e2e05bb8452f56
LivioDR <liviodavidreinoso@yahoo.com.ar>
2024-07-04 15:34:40 -0400

    added customization to ConfirmButton

--- src/components/ConfirmButton/ConfirmButton.jsx
+++ src/components/ConfirmButton/ConfirmButton.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import './ConfirmBtnStyle.css'
 
-const ConfirmButton = ({route, ready}) => {
+const ConfirmButton = ({route, ready, confirmText = 'Confirm', loadingText = 'Loading...'}) => {
     if(ready){
         return(
             <button className="confirmWrapper" onClick={route}>
-                <p className="confirmText">Confirm</p>
+                <p className="confirmText">{confirmText}</p>
             </button>
         )
     }
     else{
         return(
             <button className="confirmWrapper" disabled>
-                <p className="confirmText">Loading...</p>
+                <p className="confirmText">{loadingText}</p>
             </button>
         )
     }
