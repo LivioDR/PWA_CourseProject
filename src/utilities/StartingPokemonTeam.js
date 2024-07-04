1c750e3e1b94162e56feff91eda51561e04b7eef
LivioDR <liviodavidreinoso@yahoo.com.ar>
2024-07-03 20:15:24 -0400

    created StartingPokemonTeam for quick DB setup

---
+++ src/utilities/StartingPokemonTeam.js
@@ -0,0 +1,31 @@
+const StartingPokemonTeam = [
+    {
+        id: 1,
+        name: 'bulbasaur',
+        image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/1.gif',
+        level: 5,
+        exp: 135,
+    },
+    {
+        id: 4,
+        name: 'charmander',
+        image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/4.gif',
+        level: 5,
+        exp: 135,
+    },
+    {
+        id: 7,
+        name: 'squirtle',
+        image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/7.gif',
+        level: 5,
+        exp: 135,
+    },
+    {
+        id: 25,
+        name: 'pikachu',
+        image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/25.gif',
+        level: 5,
+        exp: 135,
+    }
+]
+export default StartingPokemonTeam
