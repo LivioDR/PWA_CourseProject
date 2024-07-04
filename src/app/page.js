c9612fed870dc2013ab83eee6b9a861b7048cbb2
LivioDR <liviodavidreinoso@yahoo.com.ar>
2024-07-03 20:46:38 -0400

    MoveSelection page created

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,27 @@
+'use client'
 import Metadata from "@/components/Metadata";
 import Header from "@/components/Header/Header";
 import SelectionPage from "@/pages/SelectionPage";
+import { useState } from "react";
+import MoveSelectionPage from "@/pages/MoveSelectionPage";
 
 export default function Home() {
+
+const [moveSelectionPage, setMoveSelectionPage] = useState(false)
+const [pokemonData, setPokemonData] = useState({})
 
   return (
     <>
       <Metadata/>
       <Header/>
-      <SelectionPage/>
+      {
+        !moveSelectionPage &&
+        <SelectionPage nextPage={()=>{setMoveSelectionPage(true)}} pokemonData={pokemonData} setPokemonData={setPokemonData}/>
+      }
+      { 
+        moveSelectionPage &&
+        <MoveSelectionPage pokemonData={pokemonData}/>
+      }
     </>
   );
 }
---
+++ src/pages/MoveSelectionPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MoveSelectionPage = ({pokemonData}) => {
+
+    return(
+        <>
+        <p>{JSON.stringify(pokemonData)}</p>
+        </>
+    )
+}
+export default MoveSelectionPage
