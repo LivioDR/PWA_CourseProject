7f63c9ed3b4e271c9cd0c90bb02442d14d0f8f5a
LivioDR <liviodavidreinoso@yahoo.com.ar>
2024-07-06 14:46:56 -0400

    Lifted moves state to Home from MoveSelectionPage. Added conditional rendering for BattlePage

--- src/app/page.js
+++ src/app/page.js
@@ -4,23 +4,33 @@ import Header from "@/components/Header/Header";
 import SelectionPage from "@/pages/SelectionPage";
 import { useState } from "react";
 import MoveSelectionPage from "@/pages/MoveSelectionPage";
+import BattlePage from "@/pages/BattlePage";
 
 export default function Home() {
 
-const [moveSelectionPage, setMoveSelectionPage] = useState(false)
-const [pokemonData, setPokemonData] = useState({})
+  const [selectionPage, setSelectionPage] = useState(true)
+  const [moveSelectionPage, setMoveSelectionPage] = useState(false)
+  const [battlePage, setBattlePage] = useState(false)
+  const [pokemonData, setPokemonData] = useState({})
+  const [selectedMoves, setSelectedMoves] = useState([])
+  const [rivalPokemonData, setRivalPokemonData] = useState({})
+
 
   return (
     <>
       <Metadata/>
       <Header/>
       {
-        !moveSelectionPage &&
-        <SelectionPage nextPage={()=>{setMoveSelectionPage(true)}} pokemonData={pokemonData} setPokemonData={setPokemonData}/>
+        selectionPage &&
+        <SelectionPage nextPage={()=>{setMoveSelectionPage(true);setSelectionPage(false)}} pokemonData={pokemonData} setPokemonData={setPokemonData}/>
       }
       { 
         moveSelectionPage &&
-        <MoveSelectionPage pokemonData={pokemonData}/>
+        <MoveSelectionPage nextPage={()=>{setBattlePage(true);setMoveSelectionPage(false)}} pokemonData={pokemonData} selectedMoves={selectedMoves} setSelectedMoves={setSelectedMoves} setRivalPokemonData={setRivalPokemonData}/>
+      }
+      {
+        battlePage &&
+        <BattlePage pokemonData={pokemonData} rivalPokemonData={rivalPokemonData} />
       }
     </>
   );
