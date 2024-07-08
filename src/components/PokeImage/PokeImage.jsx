d9666e82c57677324d24c8b2fb8eed174f3aa8cc
LivioDR <liviodavidreinoso@yahoo.com.ar>
2024-07-08 14:10:23 -0400

    xAlignment prop added

--- src/components/PokeImage/PokeImage.jsx
+++ src/components/PokeImage/PokeImage.jsx
@@ -2,26 +2,34 @@ import React from "react"
 import Image from "next/image"
 
 const styles = {
-    width: 250,
-    height: 250,
-    objectFit: 'contain',
-    margin: '20px',
+    container: {
+        height: '100%',
+        width: '100%',
+        display: 'flex',
+        alignItems: 'center',
+    },
+    image: {
+        width: 250,
+        height: 250,
+        objectFit: 'contain',
+        margin: '0 auto',
+    }
 }
 
 
-const PokeImage = ({img , style = {}}) => {
+const PokeImage = ({img , style = {}, xAlignment = "center"}) => {
 
     return(
-        <>
-        <Image
-        src={img}
-        width={style.width | 250}
-        height={style.height | 250}
-        unoptimized
-        style={{...styles, ...style}}
-        alt={"Pokemon sprite"}
-        />
-        </>
+        <div style={{...styles.container, justifyContent: xAlignment}}>
+            <Image
+            src={img}
+            width={style.width | 250}
+            height={style.height | 250}
+            unoptimized
+            style={{...styles.image, ...style}}
+            alt={"Pokemon sprite"}
+            />
+        </div>
     )
 }
 export default PokeImage
