06226e6ff39b399c6d59ec3ce9ac418af979306b
LivioDR <liviodavidreinoso@yahoo.com.ar>
2024-07-09 22:28:28 -0400

    fixed metadata tittle

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,8 +4,8 @@ import "./globals.css";
 const pressStart = Press_Start_2P({ weight: '400', subsets: ["latin"] });
 
 export const metadata = {
-  title: "Pokemon Battle Simulator",
-  description: "Battle simulator game developed by Livio Reinoso, as part of INFO6129 Cross Platform Mobile Development for Fanshawe College MAP1 Program",
+  title: "Pokemon Battle RPG",
+  description: "Role-Playing Game battle simulator game developed by Livio Reinoso, as part of INFO6129 Cross Platform Mobile Development for Fanshawe College MAP1 Program",
 };
 
 export default function RootLayout({ children }) {
