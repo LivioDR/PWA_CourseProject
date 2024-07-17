64e0287d9edd4aa26b187da7918fee4b113aeeef
LivioDR <liviodavidreinoso@yahoo.com.ar>
2024-07-16 21:00:39 -0400

    moved placeholder to own file

---
+++ src/utilities/testingPlaceholder.js
@@ -0,0 +1,150 @@
+const testingPlaceholder = {
+    team_1: [
+        {
+            id: 2,
+            name: 'ivysaur',
+            image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/2.gif',
+            level: 25,
+            exp: 12000,
+        },
+        {
+            id: 5,
+            name: 'charmeleon',
+            image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/5.gif',
+            level: 25,
+            exp: 12000,
+        },
+        {
+            id: 8,
+            name: 'wartortle',
+            image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/8.gif',
+            level: 25,
+            exp: 12000,
+        },
+        {
+            id: 25,
+            name: 'pikachu',
+            image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/25.gif',
+            level: 25,
+            exp: 12000,
+        },
+        {
+            id: 133,
+            name: 'eevee',
+            image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/133.gif',
+            level: 25,
+            exp: 12000,
+        },
+        {
+            id: 150,
+            name: 'mewtwo',
+            image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/150.gif',
+            level: 50,
+            exp: 125000,
+        }
+    ],
+    team_2: [
+        {
+            id: 2,
+            name: 'ivysaur',
+            image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/2.gif',
+            level: 25,
+            exp: 12000,
+        },
+        {
+            id: 5,
+            name: 'charmeleon',
+            image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/5.gif',
+            level: 25,
+            exp: 12000,
+        },
+        {
+            id: 8,
+            name: 'wartortle',
+            image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/8.gif',
+            level: 25,
+            exp: 12000,
+        },
+        {
+            id: 25,
+            name: 'pikachu',
+            image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/25.gif',
+            level: 25,
+            exp: 12000,
+        }
+    ],
+    team_3 : [
+        {
+            id: 2,
+            name: 'ivysaur',
+            image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/2.gif',
+            level: 25,
+            exp: 12000,
+        },
+        {
+            id: 5,
+            name: 'charmeleon',
+            image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/5.gif',
+            level: 25,
+            exp: 12000,
+        },
+        {
+            id: 8,
+            name: 'wartortle',
+            image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/8.gif',
+            level: 25,
+            exp: 12000,
+        },
+        {
+            id: 25,
+            name: 'pikachu',
+            image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/25.gif',
+            level: 25,
+            exp: 12000,
+        },
+        {
+            id: 130,
+            name: 'gyarados',
+            image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/130.gif',
+            level: 25,
+            exp: 12000,
+        },
+        {
+            id: 133,
+            name: 'eevee',
+            image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/133.gif',
+            level: 25,
+            exp: 12000,
+        },
+        {
+            id: 142,
+            name: 'aerodactyl',
+            image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/142.gif',
+            level: 25,
+            exp: 12000,
+        },
+        {
+            id: 147,
+            name: 'dratini',
+            image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/147.gif',
+            level: 1,
+            exp: 1,
+        },
+        {
+            id: 148,
+            name: 'dragonair',
+            image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/148.gif',
+            level: 25,
+            exp: 12000,
+        },
+        {
+            id: 150,
+            name: 'mewtwo',
+            image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/150.gif',
+            level: 50,
+            exp: 125000,
+        }
+    ]
+}
+
+export default testingPlaceholder
