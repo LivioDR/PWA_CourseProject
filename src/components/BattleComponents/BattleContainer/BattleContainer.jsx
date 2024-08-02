f1b09d9c4c4dfacb3558f3550cb4c74824a940ff
LivioDR <liviodavidreinoso@yahoo.com.ar>
2024-08-02 14:41:27 -0400

    changed battle background image

--- src/components/BattleComponents/BattleContainer/BattleContainer.jsx
+++ src/components/BattleComponents/BattleContainer/BattleContainer.jsx
@@ -13,28 +13,34 @@ const wrapperStyle = {
     width: '100%',
     maxWidth: '500px',
     margin: '0 auto',
+    backgroundImage: 'url("./assets/images/battleBackground.gif")',
+    backgroundPosition: '55% 100%',
+    backgroundSize: '800px 600px',
+    backgroundRepeat: 'no-repeat',
 }
 
 
 const BattleContainer = ({pokemonData, rivalPokemonData, battleText}) => {
     return(
-        <div className="battleContainerWrapper" style={wrapperStyle}>
-            <div className="battleContainerRow" id="battleRivalGraph" style={{display: 'grid'}}>
-                <HpBar stats={rivalPokemonData.baseStats} reduceToLeft={true} />
-                <div className="imageBattleContainer" style={{justifySelf: 'end'}}>
-                    <PokeImage img={rivalPokemonData.front_image || '/assets/images/rivalPokemonPlaceholder.png'} style={{...imageStyle, height: '50%'}} xAlignment="end" />
+        <>
+            <div className="battleContainerWrapper" style={wrapperStyle}>
+                <div className="battleContainerRow" id="battleRivalGraph" style={{display: 'grid'}}>
+                    <HpBar stats={rivalPokemonData.baseStats} reduceToLeft={true} />
+                    <div className="imageBattleContainer" style={{justifySelf: 'end'}}>
+                        <PokeImage img={rivalPokemonData.front_image || '/assets/images/rivalPokemonPlaceholder.png'} style={{...imageStyle, height: '50%'}} xAlignment="end" />
+                    </div>
+                </div>
+                <div className="battleContainerRow" id="battlePokemonGraph" style={{display: 'grid'}}>
+                    <div className="imageBattleContainer" style={{justifySelf: 'start'}}>
+                        <PokeImage img={pokemonData.back_image || '/assets/images/pokemonBackPlaceholder.gif'} style={imageStyle} xAlignment="start" />
+                    </div>
+                    <HpBar stats={pokemonData.baseStats} reduceToLeft={false} />
                 </div>
             </div>
-            <div className="battleContainerRow" id="battlePokemonGraph" style={{display: 'grid'}}>
-                <div className="imageBattleContainer" style={{justifySelf: 'start'}}>
-                    <PokeImage img={pokemonData.back_image || '/assets/images/pokemonBackPlaceholder.gif'} style={imageStyle} xAlignment="start" />
-                </div>
-                <HpBar stats={pokemonData.baseStats} reduceToLeft={false} />
-            </div>
-            <div className="battleContainerRow" id="battleTextGraph" style={{width: '90%', display: 'flex', flexDirection: 'row', justifyContent: 'flex-start', margin: '5%',}}>
+            <div className="battleContainerRow" id="battleTextGraph" style={{width: '100%', maxWidth: '500px', display: 'flex', flexDirection: 'row', justifyContent: 'flex-start', margin: '0 auto', marginTop: '20px', paddingInline: '2%'}}>
                 <BattleText text={battleText} />
             </div>
-        </div>
+        </>
     )
 }
 export default BattleContainer
