9f13a4c6448a7e4ae4deecb0291425403c87c364
LivioDR <liviodavidreinoso@yahoo.com.ar>
2024-08-03 21:17:23 -0400

    added IndexedDB fallback for Firebase data

--- src/database/firebaseFunctions.js
+++ src/database/firebaseFunctions.js
@@ -116,22 +116,93 @@ const createCollectionForUserId = async(uid = testUid, username = testUsername) 
 
 const getCollectionForUserId = async(uid = testUid) => {
     let pokeCollection = []
-    try{
-        const querySnapshot = await getDocs(collection(db, 'pokedex'))
-        querySnapshot.forEach(document => {
-            if(document.id == uid){
-                const retrievedPokedex = document.data().pokedex
-                for(const [key, val] of Object.entries(retrievedPokedex)){
-                    pokeCollection.push({
-                        id: key,
-                        ...val,
-                    })
+
+    // if I have internet connection I retrieve the data from Firebase
+    if(navigator.onLine){
+        try{
+            const querySnapshot = await getDocs(collection(db, 'pokedex'))
+            querySnapshot.forEach(document => {
+                if(document.id == uid){
+                    const retrievedPokedex = document.data().pokedex
+                    for(const [key, val] of Object.entries(retrievedPokedex)){
+                        pokeCollection.push({
+                            id: key,
+                            ...val,
+                        })
+                    }
+                    // then I save this data in the indexedDB as a temporary backup
+                    const request = window.indexedDB.open("pokemon",1)
+                    request.onerror = () => {
+                        console.error("An error occurred while trying to open the database")
+                    }
+                    request.onsuccess = (event) => {
+                        // get the database
+                        const db = event.target.result
+
+                        // start a RW transaction
+                        const transaction = db.transaction("pokedex","readwrite")
+
+                        transaction.oncomplete = () => {
+                            console.log("Pokedex stored in the IndexedDB")
+                        }
+                        transaction.onerror = (event) => {
+                            console.error("An error occurred in the IndexedDB transaction")
+                            console.error(event)
+                        }
+
+                        // then I get the object store
+                        const objectStore = transaction.objectStore("pokedex")
+                        const request = objectStore.put(pokeCollection, uid)
+
+                        request.onsuccess = (event) => {
+                            console.log("Request completed successfully")
+                            console.log(event)
+                        }
+                    }
+
+                    request.onupgradeneeded = (event) => {
+                        const db = event.target.result
+                        const objectStore = db.createObjectStore("pokedex")
+                    }
                 }
-            }
-        })
+            })
+        }
+        catch(e){
+            console.error(e)
+        }
     }
-    catch(e){
-        console.error(e)
+    // if I don't have an internet connection
+    else{
+        // retrieve the data from the IndexedDB
+        const request = window.indexedDB.open("pokemon",1)
+        request.onerror = () => {
+            console.error("An error occurred while trying to open the database")
+        }
+        request.onsuccess = (event) => {
+            // get the database
+            const db = event.target.result
+
+            // start a read only transaction
+            const transaction = db.transaction("pokedex","readonly")
+
+            transaction.oncomplete = () => {
+                console.log("Pokedex stored in the IndexedDB")
+            }
+            transaction.onerror = (event) => {
+                console.error("An error occurred in the IndexedDB transaction")
+                console.error(event)
+            }
+
+            // then I get the object store
+            const objectStore = transaction.objectStore("pokedex")
+            const request = objectStore.get(uid)
+
+            // and store the IndexedDB data in the variable to return
+            request.onsuccess = (event) => {
+                pokeCollection = event.result
+                console.log("Request completed successfully")
+            }
+        }
     }
     return pokeCollection
 }
@@ -147,11 +218,46 @@ const updateCollectionForUserId = async(uid = testUid, collection) => {
     const dataToUpload = {
         pokedex: pokedex
     }
-    try{
-        await updateDoc(doc(db, 'pokedex', uid), dataToUpload)
-    }   
-    catch(e){
-        console.error(e)
+
+    // if there's an online connection update the info in Firebase
+    if(navigator.onLine){
+        try{
+            await updateDoc(doc(db, 'pokedex', uid), dataToUpload)
+        }   
+        catch(e){
+            console.error(e)
+        }
+    }
+
+    // Also update the info in the IndexedDB
+    const request = window.indexedDB.open("pokemon",1)
+    request.onerror = () => {
+        console.error("An error occurred while trying to open the database")
+    }
+    request.onsuccess = (event) => {
+        // get the database
+        const db = event.target.result
+
+        // start a RW transaction
+        const transaction = db.transaction("pokedex","readwrite")
+
+        transaction.oncomplete = () => {
+            console.log("Pokedex updated in the IndexedDB")
+        }
+        transaction.onerror = (event) => {
+            console.error("An error occurred in the IndexedDB transaction")
+            console.error(event)
+        }
+
+        // then I get the object store
+        const objectStore = transaction.objectStore("pokedex")
+        // and update the info
+        const request = objectStore.put(collection, uid)
+
+        request.onsuccess = (event) => {
+            console.log("Request completed successfully")
+            console.log(event)
+        }
     }
 }
 
