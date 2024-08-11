279c8c4c12419619c734d0a3c15af420c2c239ba
LivioDR <liviodavidreinoso@yahoo.com.ar>
2024-08-11 12:33:41 -0400

    removed Auth functions wrapped debug comments in if clauses changed add method to put in indexedDB requests

--- src/database/firebaseFunctions.js
+++ src/database/firebaseFunctions.js
@@ -1,12 +1,11 @@
 // Import the functions you need from the SDKs you need
 import StartingPokemonTeam from "@/utilities/StartingPokemonTeam";
 import { initializeApp } from "firebase/app";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import { getFirestore, setDoc, doc, updateDoc, collection, getDocs } from "firebase/firestore";
-
 
 const testUid = "qwertyuiopasdfghjkl"
 const testUsername = "UsernameForTesting"
+const debug = false
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -23,81 +22,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app)
 const db = getFirestore(app)
 
-const signUp = (username, email, password) => {
-    if(isUsernameAvailable()){
-        createUserWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-            const user = userCredential.user;
-            if(typeof window != "undefined"){
-                localStorage.setItem('uid',user.uid)
-                localStorage.setItem('username',username)
-            }
-            // then create the pokedex in the database
-            createCollectionForUserId(user.uid, username)
-        })
-        .catch((error) => {
-            console.error(error.code)
-            console.error(error.message)    
-        });
-        return true // signUp successfull
-    }
-    return false // username not available
-}
-
-const signIn = async(email, password) => {
-    let uid
-    signInWithEmailAndPassword(auth, email, password)
-    .then(credential => {
-        uid = credential.user.uid
-        if(typeof window != "undefined"){
-            localStorage.setItem("uid",uid)
-        }    
-    })
-    .catch(error => {
-        console.error(error.code)
-        console.error(error.message)
-    })
-    try{
-        const querySnapshot = await getDocs(collection(db, 'pokedex'))
-        querySnapshot.forEach(document => {
-            if(document.id == uid){
-                const username = document.data().username
-                if(typeof window != "undefined"){
-                    localStorage.setItem("username",username)
-                }
-            }
-        })
-    }
-    catch(e){
-        console.error(e)
-    }
-    return true
-}
-
-// TODO: refactor function
-const isUsernameAvailable = async(username) => {
-    let isAvailable = true
-    let user = username.toLowerCase().trim()
-    await db()
-        .collection("users")
-        // .where("user","==",user)
-        .get()
-        .then(querySnapshot => {
-            querySnapshot.forEach(doc => {
-                if(doc?.data()?.user.length > 0 && doc?.data()?.user.toLowerCase() === user){
-                    isAvailable = false
-                }
-            })
-        })
-        .catch(e => {
-            console.error(e)
-            isAvailable = false
-        })
-    return isAvailable
-}
 
 const createCollectionForUserId = async(uid = testUid, username = testUsername) => {
     const initialPokedex = StartingPokemonTeam
@@ -143,7 +69,9 @@ const getCollectionForUserId = async(uid = testUid) => {
                         const transaction = db.transaction("pokedex","readwrite")
 
                         transaction.oncomplete = () => {
-                            console.log("Pokedex stored in the IndexedDB")
+                            if(debug){
+                                console.log("Pokedex stored in the IndexedDB")
+                            }
                         }
                         transaction.onerror = (event) => {
                             console.error("An error occurred in the IndexedDB transaction")
@@ -152,11 +80,13 @@ const getCollectionForUserId = async(uid = testUid) => {
 
                         // then I get the object store
                         const objectStore = transaction.objectStore("pokedex")
-                        const request = objectStore.add(pokeCollection, uid)
+                        const request = objectStore.put(pokeCollection, uid)
 
                         request.onsuccess = (event) => {
-                            console.log("Request completed successfully")
-                            console.log(event)
+                            if(debug){
+                                console.log("Request completed successfully")
+                                console.log(event)
+                            }
                         }
                     }
 
@@ -186,7 +116,9 @@ const getCollectionForUserId = async(uid = testUid) => {
             const transaction = db.transaction(["pokedex"],"readonly")
 
             transaction.oncomplete = () => {
-                console.log("Pokedex retrieved from the IndexedDB")
+                if(debug){
+                    console.log("Pokedex retrieved from the IndexedDB")
+                }
             }
             transaction.onerror = (event) => {
                 console.error("An error occurred in the IndexedDB transaction")
@@ -200,7 +132,9 @@ const getCollectionForUserId = async(uid = testUid) => {
             // and store the IndexedDB data in the variable to return
             request.onsuccess = (event) => {
                 pokeCollection = event.target.result
-                console.log("Request completed successfully")
+                if(debug){
+                    console.log("Request completed successfully")
+                }
             }
         }
     }
@@ -241,7 +175,9 @@ const updateCollectionForUserId = async(uid = testUid, collection) => {
             const transaction = db.transaction("pokedex","readwrite")
     
             transaction.oncomplete = () => {
-                console.log("Pokedex updated in the IndexedDB")
+                if(debug){
+                    console.log("Pokedex updated in the IndexedDB")
+                }
             }
             transaction.onerror = (event) => {
                 console.error("An error occurred in the IndexedDB transaction")
@@ -251,14 +187,16 @@ const updateCollectionForUserId = async(uid = testUid, collection) => {
             // then I get the object store
             const objectStore = transaction.objectStore("pokedex")
             // and update the info
-            const request = objectStore.add(collection, uid)
+            const request = objectStore.put(collection, uid)
     
             request.onsuccess = (event) => {
-                console.log("Request completed successfully")
-                console.log(event)
+                if(debug){
+                    console.log("Request completed successfully")
+                    console.log(event)
+                }
             }
         }
     }
 }
 
-export { signIn, signUp, createCollectionForUserId, getCollectionForUserId, updateCollectionForUserId }
+export { createCollectionForUserId, getCollectionForUserId, updateCollectionForUserId }
